3b6a31e64d60b3f79bc599354eacc738ccf940a6
Tracy Goldman <tgoldman413@gmail.com>
2026-01-26 17:19:24 -0500

    wrap guests, create grid container

--- scenes/party.gd
+++ scenes/party.gd
@@ -1,13 +1,26 @@
 extends Node
 
 @onready var door: Button = $Door
+@onready var house_grid: GridContainer = $HouseGrid
 @export var guest: PackedScene
+@export var start_pos: Vector2 = Vector2(218, 68)
+@export var x_diff: int = 150
+@export var y_diff: int = 150
+@export var edge: int = 1000
 
-var next_pos = Vector2(150,100)
+var curr_pos = start_pos
+
+func _ready() -> void:
+	print(door.size)
 	
 
 func _on_door_pressed() -> void:
+	print(curr_pos)
 	var new_guest = guest.instantiate()
-	add_child(new_guest)
-	new_guest.position = next_pos
-	next_pos.x += 150
+	house_grid.add_child(new_guest)
+	#add_child(new_guest)
+	new_guest.position = curr_pos
+	curr_pos.x += x_diff
+	if curr_pos.x > edge:
+		curr_pos.x = start_pos.x
+		curr_pos.y += y_diff
