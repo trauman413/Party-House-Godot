144a1f807c4579fa297d85f170dbc134d1933e0c
Tracy Goldman <tgoldman413@gmail.com>
2026-02-12 16:14:14 -0500

    somehow new variable name fixed issues????

--- scenes/party.gd
+++ scenes/party.gd
@@ -5,7 +5,7 @@ extends Node
 @onready var party_controller: Node = $PartyController
 @onready var cat: Sprite2D = $Cat/Sprite2D
 
-@export var guest: PackedScene
+@export var guest_data: PackedScene
 @export var start_pos: Vector2 = Vector2(218, 136)
 @export var x_diff: int = 150
 @export var y_diff: int = 150
@@ -23,7 +23,7 @@ func _ready() -> void:
 
 func _on_door_pressed() -> void:
 	if curr_house_members < party_controller.house_size:
-		var new_guest = guest.instantiate()
+		var new_guest = guest_data.instantiate()
 		house_grid.add_child(new_guest)
 		curr_house_members += 1
 		new_guest.position = curr_pos
